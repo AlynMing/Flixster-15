0139b9c60beadbe1d7bff030e44f78a593048ea7
MahdyahHassanyar <msayedtahershah1@student.gsu.edu>
2020-10-02 20:58:12 -0400

    A new page with title,rating, overview, and trailer.

--- app/src/main/java/com/example/flixster/Models/Movie.java
+++ app/src/main/java/com/example/flixster/Models/Movie.java
@@ -3,20 +3,29 @@ package com.example.flixster.Models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 import java.util.ArrayList;
 import java.util.List;
-
+@Parcel
 public class Movie {
+    int movieId;
     String backdropPath;
     String posterPath;
     String title;
     String overView;
+    double rating;
+    // empty constructer is needed for parcel
+    public Movie(){
+
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
+        movieId=jsonObject.getInt("id");
         backdropPath=jsonObject.getString("backdrop_path");
         posterPath= jsonObject.getString("poster_path");
         title=jsonObject.getString("title");
         overView=jsonObject.getString("overview");
+        rating=jsonObject.getDouble("vote_average");
     }
     public static List<Movie>fromJsonArray(JSONArray movieJsonArray) throws JSONException {
         List<Movie> movies=new ArrayList<>();
@@ -40,5 +49,11 @@ public class Movie {
     public String getOverView() {
 
         return overView;
+    }
+    public double getRating(){
+        return rating;
+    }
+    public int getMovieId(){
+        return movieId;
     }
 }
--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -1,19 +1,26 @@
 package com.example.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.Parcel;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import java.util.List;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
+import com.example.flixster.DetailActivity;
 import com.example.flixster.Models.Movie;
 import com.example.flixster.R;
 import android.util.Log;
+import android.widget.Toast;
+
+import org.parceler.Parcels;
 
 public class MovieAdapter extends RecyclerView.Adapter <MovieAdapter.ViewHolder>{
     Context context;
@@ -42,6 +49,7 @@ public class MovieAdapter extends RecyclerView.Adapter <MovieAdapter.ViewHolder>
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        RelativeLayout container;
         TextView tvTittle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -51,9 +59,10 @@ public class MovieAdapter extends RecyclerView.Adapter <MovieAdapter.ViewHolder>
             tvTittle=itemView.findViewById(R.id.tvTitle);
             tvOverview=itemView.findViewById(R.id.tvOverview);
             ivPoster=itemView.findViewById(R.id.ivPoster);
+            container=itemView.findViewById(R.id.container);
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTittle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverView());
             String imageUrl ;
@@ -63,6 +72,18 @@ public class MovieAdapter extends RecyclerView.Adapter <MovieAdapter.ViewHolder>
                 imageUrl=movie.getPosterPath();
             }
             Glide.with(context).load(imageUrl).into(ivPoster);
+            // register click listener on the whole row
+            // navigate to new activity
+           container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent i=new Intent(context, DetailActivity.class);
+
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+
+                }
+            });
         }
     }
 }
